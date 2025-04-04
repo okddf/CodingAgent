1e503b70362d4e9d857179dd9c893009b1f37b8a
Kónya Gergely <konya.gergely.13@gmail.com>
2025-04-04 19:30:14 +0200

    env

--- llm.py
+++ llm.py
@@ -1,5 +1,8 @@
 from langchain_openai import ChatOpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
 
-llm = ChatOpenAI(model= "gpt-4o-mini", api_key= "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")
+load_dotenv()
+
+llm = ChatOpenAI(model= "gpt-4o-mini")
 #llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", api_key="AIzaSyDQwjpqNdt6UEN0ksC6GybPXXoarmyUsXo")
